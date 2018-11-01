fa43b1c20adabc2f647b4229a74dc5127ac9fb10
ABDUL KADHAR <43913066+abdulkadhar0435@users.noreply.github.com>
2018-11-01 19:46:26 +0300

    Update views/_layout_main.php

--- views/_layout_main.php
+++ views/_layout_main.php
@@ -1,16 +1,5 @@
-<?php $this->load->myview('views','components/page_header'); ?>
+<?php $this->load->view('components/page_header'); ?>
 <?php $this->load->view("components/page_topbar"); ?>
 <?php $this->load->view("components/page_menu"); ?>
-<?php $this->load->myview('views',$subview); ?>
-        <!-- <aside class="right-side">
-            <section class="content">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <?php //$this->load->view($subview); ?> 
-                    </div>
-                </div>
-            </section>
-        </aside> -->
-                            
-
-<?php $this->load->myview('views','components/page_footer'); ?>
+<?php $this->load->view('views',$subview); ?>
+<?php $this->load->view('components/page_footer'); ?>
